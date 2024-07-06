6db50620164ab4dcf7172d69d5c2e339087678c9
Lucas <luucasmeedeiros@gmail.com>
2024-07-06 16:30:31 -0300

    fix: health

--- src/main/java/com/fiap/checkout/infraestructure/api/controllers/HealthController.java
+++ src/main/java/com/fiap/checkout/infraestructure/api/controllers/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/checkout")
+@RequestMapping("/")
 public class HealthController {
     @GetMapping("/healthz")
     public ResponseEntity<String> healthz() {
